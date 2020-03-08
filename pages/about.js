9cb82e0ca9bdd6e688ee09d69127c79e3267a554
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-08 22:34:47 +0800

    [refactor] Add core skills to about page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,22 @@
+import CoreSkills from '../components/CoreSkills';
+
 const about = () => {
-  return <div className="text-app-slate">Under Construction</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="w-full px-4 md:px-6 text-xl leading-normal">
+          <h1 className="text-app-lightestSlate font-bold font-sans break-normal mb-6 mt-2 text-3xl md:text-4xl">
+            Core Skills
+          </h1>
+          <CoreSkills />
+          <h1 className="text-app-lightestSlate font-bold font-sans break-normal mb-6 mt-4 text-3xl md:text-4xl">
+            Where I've worked
+          </h1>
+          <p className="text-app-slate">Under Construction.</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default about;
