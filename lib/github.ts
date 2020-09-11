008d08ba8d3b7aa32e4c6e71896632d11eacd70f
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-09-11 20:02:13 +0800

    Fix missing export err

--- lib/github.ts
+++ lib/github.ts
@@ -22,28 +22,26 @@ const GET_REPOSITORIES = ` query {
 }
 `;
 
-module.exports = {
-  async getRepositories() {
-    try {
-      const response = await axios({
-        url: 'https://api.github.com/graphql',
-        method: 'post',
-        headers: {
-          Authorization: `Bearer ${process.env.NEXT_SERVER_GITHUB_PERSONAL_ACCESS_TOKEN}`,
-        },
-        data: {
-          query: GET_REPOSITORIES,
-        },
-      });
+export async function getRepositories() {
+  try {
+    const response = await axios({
+      url: 'https://api.github.com/graphql',
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_SERVER_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+      data: {
+        query: GET_REPOSITORIES,
+      },
+    });
 
-      if (response.data.errors) {
-        throw new Error(response.data.errors);
-      }
-
-      return response.data.data.user.repositories.nodes;
-    } catch (e) {
-      console.log(e);
-      throw new Error(e);
+    if (response.data.errors) {
+      throw new Error(response.data.errors);
     }
-  },
-};
+
+    return response.data.data.user.repositories.nodes;
+  } catch (e) {
+    console.log(e);
+    throw new Error(e);
+  }
+}
