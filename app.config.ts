a39bf8a003b05894eec574e98e30159b31ca450e
wobsoriano <sorianorobertc@gmail.com>
2023-06-24 21:35:43 -0700

    use content wind template

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  // Configure Content Wind here
+  cover: '/cover.jpg',
+  socials: {
+    twitter: 'wobsoriano',
+    github: 'wobsoriano'
+  },
+  prose: {
+    h1: {
+      icon: 'carbon:sailboat-coastal'
+    }
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode'
-  ],
-  // https://color-mode.nuxtjs.org
-  colorMode: {
-    classSuffix: ''
-  },
-  // https://content.nuxtjs.org
-  content: {
-    documentDriven: true,
-    navigation: {
-      fields: ['navTitle']
-    },
-    highlight: {
-      // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
-      theme: 'dracula'
-    }
-  },
-  build: {
-    // transpile: ['vue-pdf-embed']
-  }
+  extends: 'content-wind'
 })
